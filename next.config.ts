eaf2317ccbcea307079020873f4c62a87c20ef88
Girish <girishvelaga20@example.com>
2026-06-29 08:31:42 +0530

    Fix Google profile image loading

--- next.config.ts
+++ next.config.ts
@@ -9,23 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
